3ba6c17c60d37d66bce79c6c59a9125c3ea2749c
Oleksandr1301 <viss82@ukr.net>
2023-01-08 18:17:48 +0200

    add lightbox

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const paletContainer = document.querySelector(".gallery");
+const imgMarkup = createImgGalleryMarkup(galleryItems);
+
+paletContainer.insertAdjacentHTML("beforeend", imgMarkup);
+
+// console.log(createImgGalleryMarkup(galleryItems));
+function createImgGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+                />
+            </a>`;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+    captionsData: "alt"
+});
