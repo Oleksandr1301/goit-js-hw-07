d21f6d2d2b0e8333b8e72b4f196af78a7bdb8343
Oleksandr1301 <viss82@ukr.net>
2023-01-08 12:50:53 +0200

    add html

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const paletContainer = document.querySelector('.gallery');
+const imgMarkup = createImgGalleryMarkup(galleryItems);
 
-console.log(galleryItems);
+paletContainer.insertAdjacentHTML('beforeend', imgMarkup);
+
+console.log(createImgGalleryMarkup(galleryItems));
+function createImgGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${preview}">
+                <img
+                class="gallery__image"
+                src="${original}"
+                data-source="${preview}"
+                alt="${description}"
+                />
+            </a>
+        </div>`;
+    })
+    .join("");
+}
