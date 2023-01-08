914372c694e8e0da635286293440ef633e7cd34e
Oleksandr1301 <viss82@ukr.net>
2023-01-08 17:50:36 +0200

    add gallery1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,25 +1,49 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const paletContainer = document.querySelector('.gallery');
+const paletContainer = document.querySelector(".gallery");
 const imgMarkup = createImgGalleryMarkup(galleryItems);
 
-paletContainer.insertAdjacentHTML('beforeend', imgMarkup);
+paletContainer.insertAdjacentHTML("beforeend", imgMarkup);
 
-console.log(createImgGalleryMarkup(galleryItems));
+// console.log(createImgGalleryMarkup(galleryItems));
 function createImgGalleryMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
         <div class="gallery__item">
-            <a class="gallery__link" href="${preview}">
+            <a class="gallery__link" href="${original}">
                 <img
                 class="gallery__image"
-                src="${original}"
-                data-source="${preview}"
+                src="${preview}"
+                data-source="${original}"
                 alt="${description}"
                 />
             </a>
         </div>`;
     })
     .join("");
+}
+
+paletContainer.addEventListener("click", openImgEl);
+// import * as basicLightbox from 'basiclightbox';
+function openImgEl(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.getAttribute(
+      "data-source"
+    )}" width="800" height="600">
+`);
+
+  instance.show();
+
+  paletContainer.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      instance.close();
+    }
+  });
 }
